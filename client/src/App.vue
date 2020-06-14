f42701916ddaf6014dca942d62df468b5cfd1daf
Marcos Alexandre V. Costa <marcos@substractum.com.br>
2020-06-14 13:44:12 -0300

    server and client

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQl e Node</h6>
+    </div>
+
+    <div id="main">
+      <DomainList></DomainList>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList";
+
+export default {
+  name: "App",
+  components: { DomainList }
+};
+</script>
+
+<style>
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
